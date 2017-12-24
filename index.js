26179d5e67db288472063abe05267c25d1edfb92
winter mutt <sejongsan@hotmail.com>
2017-12-24 20:12:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign(object, { [key]: value});
 }
+
+function deleteFromObjectByKey(object, key){
+  delete Object.assign({}, object.key);
+  return object;
+}
